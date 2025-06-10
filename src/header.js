9c1a160286e93c781fbdd54f0bd509a377be2ea8
Adanta Griffin <griffinadanta90@gmail.com>
2025-06-10 09:56:44 -0400

    edit

--- src/header.js
+++ src/header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header(props){
   return (
     <div>
-      <img className="header" alt="" src={props.profileImg}></img>
+      <img className="header" alt="photo" src={props.profileImg}></img>
       <h1>{props.username}</h1>
     </div>
   )

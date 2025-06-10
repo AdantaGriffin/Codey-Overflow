354cfd3c631adbba34743bbaa01ed98fe662e38d
Adanta Griffin <griffinadanta90@gmail.com>
2025-06-10 09:54:31 -0400

    edit for deploy

--- src/header.js
+++ src/header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header(props){
   return (
     <div>
-      <img className="header" src={props.profileImg}></img>
+      <img className="header" alt="" src={props.profileImg}></img>
       <h1>{props.username}</h1>
     </div>
   )
